d7f0c0d7300e5ba362b560a90d950895a0eb6d3f
Josh Baker <joshualbaker111@gmail.com>
2021-06-30 13:03:12 -0400

    add dotfiles

---
+++ setup.sh
@@ -0,0 +1,16 @@
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# Install Oh My Zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Remove old configuration
+mv ~/.zshrc ~/.zshrc.old
+
+# Link Zsh config
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+
+# Reload zsh config
+source ~/.zshrc
